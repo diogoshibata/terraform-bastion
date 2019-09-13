cbad0f689d6d115be7109ae843d42e44b5c391d6
diogoshibata <35370798+diogoshibata@users.noreply.github.com>
2019-09-13 15:59:08 -0300

    Update

--- terraform/oke.tf
+++
@@ -1,8 +0,0 @@
-resource "oci_containerengine_cluster" "oke_cluster" {
-    depends_on = ["oci_identity_policy.allow_oke_policy"]
-    
-    compartment_id = "${oci_identity_compartment.oke_compartment.id}"
-    kubernetes_version = "1.13.5"
-    name = "cluster-workshop"
-    vcn_id = "${oci_core_vcn.vcn-workshop.id}"
-}
